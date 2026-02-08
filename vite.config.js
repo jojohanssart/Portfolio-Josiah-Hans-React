e7e4200aa2bedeb1b6f96b6ed298dd2af0e5fcd9
Josiah Hans <jojohanss.art@gmail.com>
2026-02-08 21:33:06 +0700

    Setup and deploy to github pages. Adjust avatar and menu overlay

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Portfolio-Josiah-Hans-React"
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
